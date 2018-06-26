12625fd76742c46fa263e50f379c6b37c936fae0
Amit Pundir <amit.pundir@linaro.org>
2018-06-26 22:58:04 +0530

    utils.mk: Add a common utils.mk file for build/makefile helper Macros
    Instead of redefining add-qcom-firmware() for every firmware installer
    makefile, have a single common definition and use that instead.
    
    In future put all common build/makefile Macros in this utils.mk.
    
    Change-Id: I0a15491133c9812bc91dee9692849017fc99e4c2
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>

--- db410c32_only/firmware/Android.mk
+++ db410c32_only/firmware/Android.mk
@@ -1,18 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-# $(1): The source file name in LOCAL_PATH.
-#       It also serves as the module name and the dest file name.
-# $(2): Module installation path.
-define add-qcom-firmware
-$(eval include $(CLEAR_VARS))\
-$(eval LOCAL_MODULE := $(1))\
-$(eval LOCAL_SRC_FILES := $(1))\
-$(eval LOCAL_MODULE_STEM := $(1))\
-$(eval LOCAL_MODULE_CLASS := FIRMWARE)\
-$(eval LOCAL_MODULE_TAGS := optional)\
-$(eval LOCAL_MODULE_PATH := $(2))\
-$(eval include $(BUILD_PREBUILT))
-endef
+include device/linaro/dragonboard/utils.mk
 
 # Firmware files copied from
 # http://builds.96boards.org/releases/dragonboard410c/qualcomm/firmware/linux-board-support-package-r1032.1.1.zip
--- db820c/firmware/ath10k/QCA6174/hw3.0/Android.mk
+++ db820c/firmware/ath10k/QCA6174/hw3.0/Android.mk
@@ -1,18 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-# $(1): The source file name in LOCAL_PATH.
-#       It also serves as the module name and the dest file name.
-# $(2): Module installation path.
-define add-qcom-firmware
-$(eval include $(CLEAR_VARS))\
-$(eval LOCAL_MODULE := $(1))\
-$(eval LOCAL_SRC_FILES := $(1))\
-$(eval LOCAL_MODULE_STEM := $(1))\
-$(eval LOCAL_MODULE_CLASS := FIRMWARE)\
-$(eval LOCAL_MODULE_TAGS := optional)\
-$(eval LOCAL_MODULE_PATH := $(2))\
-$(eval include $(BUILD_PREBUILT))
-endef
+include device/linaro/dragonboard/utils.mk
 
 # Ath10k Qca6174 firmware files copied from
 # https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/tree/ath10k/
